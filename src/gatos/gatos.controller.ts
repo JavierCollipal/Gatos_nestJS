1ca25ae7cea4c49a2c3ddf54d3f58c82640b40dd
Javier Collipal <jcollipal1212@gmail.com>
2019-07-21 19:17:56 -0400

    base de controlador creado y comentado

--- src/gatos/gatos.controller.ts
+++ src/gatos/gatos.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, Req,Res } from '@nestjs/common';
+/**el string 'gatos' declarado en el class decorator Controller hace referencia 
+ * a un route path, en este caso 'gatos' esta definiendo que la ruta /gatos 
+ * va hacer referencia a este controlador
+ */
 @Controller('gatos')
-export class GatosController {}
+export class GatosController {
+    /** En este caso si dejamos el decorator de get sin añadir un prefijo
+     * cada petición get a /gatos va retornar la funcion Todos Los gatos
+    */
+
+    /**de todas formas podemos añadir un prefijo 'todosMisGatos' a get para que la
+     * ruta quede de esta forma /gatos/todosMisGatos
+     */
+    @Get()
+    TodosLosGatos(@Res() response: Response): string {
+        return 'esta accion retorna todos los gatos de la db'
+    }
+}
+/** un controlador es el encargado de manejar las  HTTP Request y dejarle las tareas complejas
+ * providers
+ */
